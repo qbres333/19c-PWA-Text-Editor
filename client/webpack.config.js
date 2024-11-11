edd99baf69005ea5281533e758b9d579a6590c59
Fin M <qbres333@hotmail.com>
2024-11-11 17:56:26 -0500

    add scripts to main package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = () => {
         display: "standalone", //metadata
         start_url: "./", //metadata
         publicPath: "./", //metadata
-        icons: [],
+        // icons: [],
       }),
     ],
 
